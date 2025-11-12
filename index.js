178a0a04a7f0419966058fa14a819ec2652c13d2
priom125 <priom6046@gmail.com>
2025-11-12 18:43:16 +0600

    favorite features post and get api done

--- index.js
+++ index.js
@@ -2,13 +2,45 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-
+const admin = require("firebase-admin");
 dotenv.config();
 const app = express();
+
+
+
+const serviceAccount = require("./assignment10-firebase-adminsdk-.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyFirebaseToken = async(req, res, next) => {
+if (!req.headers.authorization) {
+  return res.status(401).send({ message: "Unauthorized access" });
+}
+  const token = req.headers.authorization.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+
+  // Verify the token using Firebase Admin SDK
+  try {
+    const uaerInfo = await admin.auth().verifyIdToken(token);
+    console.log("Decoded Token:", uaerInfo);
+    next();
+
+  } catch (error) {
+    res.status(401).send({ message: "Unauthorized access" });
+  }
+
+  
+};
 
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, {
@@ -24,11 +56,22 @@ async function run() {
     await client.connect();
     const db = client.db("LocalEats");
     const allreview = db.collection("all-review");
+    const allfavorite = db.collection("allfavorite");
     //post review api
     app.post("/add-review", async (req, res) => {
       try {
         const newReview = req.body;
         const result = await allreview.insertOne(newReview);
+        res.send(result);
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
+    });
+    //post favorite api
+    app.post("/all-favorite", async (req, res) => {
+      try {
+        const newFavorite = req.body;
+        const result = await allfavorite.insertOne(newFavorite);
         res.send(result);
       } catch (error) {
         res.status(500).json({ message: error.message });
@@ -65,6 +108,16 @@ async function run() {
         res.status(500).json({ message: error.message });
       }
     });
+
+    //get all favorite api
+    app.get("/all-favorite", async (req, res) => {
+      try {
+        const favorite = await allfavorite.find().toArray();
+        res.json(favorite) || [];
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
+    });
     // first 6 review sort and limit
     app.get("/all-review/sort", async (req, res) => {
       try {
@@ -75,7 +128,13 @@ async function run() {
       }
     });
 // get review by email
- 
+
+app.get("/user/reviews", verifyFirebaseToken, async (req, res) => {
+  const userEmail = req.user.email;
+  const reviews = await allreview.find({ email: userEmail }).toArray();
+  res.json(reviews);
+});
+
 
     //get review by id api
     app.get("/all-review/:id", async (req, res) => {
