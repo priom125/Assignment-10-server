be64d8c6bf286dc726efa69c26df336a688cbe01
priom125 <priom6046@gmail.com>
2025-11-13 10:42:11 +0600

    all done

--- index.js
+++ index.js
@@ -6,41 +6,35 @@ const admin = require("firebase-admin");
 dotenv.config();
 const app = express();
 
-
-
 const serviceAccount = require("./assignment10-firebase-adminsdk-.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
-
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-const verifyFirebaseToken = async(req, res, next) => {
-if (!req.headers.authorization) {
-  return res.status(401).send({ message: "Unauthorized access" });
-}
-  const token = req.headers.authorization.split(" ")[1];
+// const verifyFirebaseToken = async (req, res, next) => {
+//   if (!req.headers.authorization) {
+//     return res.status(401).send({ message: "Unauthorized access" });
+//   }
+//   const token = req.headers.authorization.split(" ")[1];
 
-  if (!token) {
-    return res.status(401).send({ message: "Unauthorized access" });
-  }
+//   if (!token) {
+//     return res.status(401).send({ message: "Unauthorized access" });
+//   }
 
-  // Verify the token using Firebase Admin SDK
-  try {
-    const uaerInfo = await admin.auth().verifyIdToken(token);
-    console.log("Decoded Token:", uaerInfo);
-    next();
-
-  } catch (error) {
-    res.status(401).send({ message: "Unauthorized access" });
-  }
-
-  
-};
+//   // Verify the token using Firebase Admin SDK
+//   try {
+//     const uaerInfo = await admin.auth().verifyIdToken(token);
+//     console.log("Decoded Token:", uaerInfo);
+//     next();
+//   } catch (error) {
+//     res.status(401).send({ message: "Unauthorized access" });
+//   }
+// };
 
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, {
@@ -78,19 +72,17 @@ async function run() {
       }
     });
 
-// update by patch method 
+    // update by patch method
     app.patch("/all-review/:id", async (req, res) => {
       const id = req.params.id;
-      const updatedReview = req.body; 
+      const updatedReview = req.body;
       const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: updatedReview,
-      };  
+      };
       const result = await allreview.updateOne(filter, updateDoc);
       res.send(result);
     });
-
-
 
     //delete review api
     app.delete("/all-review/:id", async (req, res) => {
@@ -99,26 +91,24 @@ async function run() {
       const result = await allreview.deleteOne(query);
       res.send(result);
     });
-//get review api
-app.get("/all-review", async (req, res) => {
-  try {
-    const search = req.query.search || "";
-    const query = search
-      ? { foodName: { $regex: search, $options: "i" } }
-      : {};
+    //get review api
+    app.get("/all-review", async (req, res) => {
+      try {
+        const search = req.query.search || "";
+        const query = search
+          ? { foodName: { $regex: search, $options: "i" } }
+          : {};
 
-    const reviews = await allreview
-      .find(query)
-      .sort({ starRating: -1 })
-      .toArray();
+        const reviews = await allreview
+          .find(query)
+          .sort({ starRating: -1 })
+          .toArray();
 
-    res.json(reviews);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-
+        res.json(reviews);
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
+    });
 
     //get all favorite api
     app.get("/all-favorite", async (req, res) => {
@@ -141,7 +131,6 @@ app.get("/all-review", async (req, res) => {
         console.error("Error fetching favorite:", error);
         res.status(500).json({ message: "Internal server error" });
       }
-     
     });
     // get review by email
     app.get("/my-review", async (req, res) => {
@@ -154,25 +143,27 @@ app.get("/all-review", async (req, res) => {
         console.error("Error fetching favorite:", error);
         res.status(500).json({ message: "Internal server error" });
       }
-     
     });
     // first 6 review sort and limit
     app.get("/all-review/sort", async (req, res) => {
       try {
-        const reviews = await allreview.find().sort({starRating:-1}).limit(6).toArray();
+        const reviews = await allreview
+          .find()
+          .sort({ starRating: -1 })
+          .limit(6)
+          .toArray();
         res.json(reviews);
       } catch (error) {
         res.status(500).json({ message: error.message });
       }
     });
-// get review by email
+    // get review by email
 
-app.get("/user/reviews", verifyFirebaseToken, async (req, res) => {
-  const userEmail = req.user.email;
-  const reviews = await allreview.find({ email: userEmail }).toArray();
-  res.json(reviews);
-});
-
+    app.get("/user/reviews", verifyFirebaseToken, async (req, res) => {
+      const userEmail = req.user.email;
+      const reviews = await allreview.find({ email: userEmail }).toArray();
+      res.json(reviews);
+    });
 
     //get review by id api
     app.get("/all-review/:id", async (req, res) => {
